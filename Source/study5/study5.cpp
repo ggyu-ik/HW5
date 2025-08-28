dfa95eba01c5b7ab691dfe27fdc274b6ff370689
ggyu-ik <ggye0723@gmail.com>
2025-08-28 20:34:31 +0900

    s

--- Source/study5/study5.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "study5.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, study5, "study5" );
